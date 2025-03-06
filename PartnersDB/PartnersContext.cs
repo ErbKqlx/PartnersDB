9bc7fb629d7a09da1ee7b265844f696c2f3ea9ea
Курзенева Кристина <kristinakurzeneva35@gmail.com>
2025-03-06 15:57:37 +0500

    Создан макет и подключил пакеты

---
+++ PartnersDB/PartnersContext.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace PartnersDB;
+
+public partial class PartnersContext : DbContext
+{
+    public PartnersContext()
+    {
+    }
+
+    public PartnersContext(DbContextOptions<PartnersContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Partner> Partners { get; set; }
+
+    public virtual DbSet<PartnersProduct> PartnersProducts { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<TypesOfPartner> TypesOfPartners { get; set; }
+
+    public virtual DbSet<TypesOfProduct> TypesOfProducts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=Partners;Username=postgres;Password=1111");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Partner>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("Partners_pkey");
+
+            entity.Property(e => e.Inn)
+                .HasMaxLength(12)
+                .HasColumnName("INN");
+            entity.Property(e => e.Phone).HasMaxLength(20);
+
+            entity.HasOne(d => d.IdTypeOfPartnerNavigation).WithMany(p => p.Partners)
+                .HasForeignKey(d => d.IdTypeOfPartner)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("Partners_IdTypeOfPartner_fkey");
+        });
+
+        modelBuilder.Entity<PartnersProduct>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("Partners_Products_pkey");
+
+            entity.ToTable("Partners_Products");
+
+            entity.HasOne(d => d.IdPartnerNavigation).WithMany(p => p.PartnersProducts)
+                .HasForeignKey(d => d.IdPartner)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("Partners_Products_IdPartner_fkey");
+
+            entity.HasOne(d => d.IdProductNavigation).WithMany(p => p.PartnersProducts)
+                .HasForeignKey(d => d.IdProduct)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("Partners_Products_IdProduct_fkey");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("Products_pkey");
+
+            entity.Property(e => e.MinimalPrice).HasColumnType("money");
+
+            entity.HasOne(d => d.IdTypeOfProductNavigation).WithMany(p => p.Products)
+                .HasForeignKey(d => d.IdTypeOfProduct)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("Products_IdTypeOfProduct_fkey");
+        });
+
+        modelBuilder.Entity<TypesOfPartner>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("TypesOfPartners_pkey");
+
+            entity.Property(e => e.Id)
+                .UseIdentityAlwaysColumn()
+                .HasIdentityOptions(5L, null, null, null, null, null);
+        });
+
+        modelBuilder.Entity<TypesOfProduct>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("TypesOfProducts_pkey");
+
+            entity.Property(e => e.Id).HasIdentityOptions(null, null, 6L, null, null, null);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ PartnersDB/TypesOfProduct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PartnersDB;
+
+public partial class TypesOfProduct
+{
+    public short Id { get; set; }
+
+    public string TypeOfProduct { get; set; } = null!;
+
+    public decimal TypeCoefficient { get; set; }
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
