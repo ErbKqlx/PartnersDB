1ddbc98bfe749a63adacf5f931cb14eed75918dc
Курзенева Кристина <kristinakurzeneva35@gmail.com>
2025-03-06 16:02:04 +0500

    d

--- PartnersDB/Form1.Designer.cs
+++ PartnersDB/Form1.Designer.cs
@@ -29,24 +29,24 @@
         private void InitializeComponent()
         {
             flowLayoutPanel1 = new FlowLayoutPanel();
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
+            buttonAddPartner = new Button();
+            buttonUpdatePartner = new Button();
+            buttonDeletePartner = new Button();
             panel1 = new Panel();
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
             label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
             flowLayoutPanel1.SuspendLayout();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
-            flowLayoutPanel1.Controls.Add(button1);
-            flowLayoutPanel1.Controls.Add(button2);
-            flowLayoutPanel1.Controls.Add(button3);
+            flowLayoutPanel1.Controls.Add(buttonAddPartner);
+            flowLayoutPanel1.Controls.Add(buttonUpdatePartner);
+            flowLayoutPanel1.Controls.Add(buttonDeletePartner);
             flowLayoutPanel1.Dock = DockStyle.Top;
             flowLayoutPanel1.Location = new Point(10, 10);
             flowLayoutPanel1.Margin = new Padding(5);
@@ -54,38 +54,38 @@
             flowLayoutPanel1.Size = new Size(841, 71);
             flowLayoutPanel1.TabIndex = 0;
             // 
-            // button1
+            // buttonAddPartner
             // 
-            button1.BackColor = Color.White;
-            button1.Location = new Point(5, 5);
-            button1.Margin = new Padding(5, 5, 5, 5);
-            button1.Name = "button1";
-            button1.Size = new Size(228, 58);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = false;
+            buttonAddPartner.BackColor = Color.White;
+            buttonAddPartner.Location = new Point(5, 5);
+            buttonAddPartner.Margin = new Padding(5);
+            buttonAddPartner.Name = "buttonAddPartner";
+            buttonAddPartner.Size = new Size(246, 58);
+            buttonAddPartner.TabIndex = 0;
+            buttonAddPartner.Text = "Добавить партнера";
+            buttonAddPartner.UseVisualStyleBackColor = false;
             // 
-            // button2
+            // buttonUpdatePartner
             // 
-            button2.BackColor = Color.White;
-            button2.Location = new Point(243, 5);
-            button2.Margin = new Padding(5, 5, 5, 5);
-            button2.Name = "button2";
-            button2.Size = new Size(228, 58);
-            button2.TabIndex = 1;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = false;
+            buttonUpdatePartner.BackColor = Color.White;
+            buttonUpdatePartner.Location = new Point(261, 5);
+            buttonUpdatePartner.Margin = new Padding(5);
+            buttonUpdatePartner.Name = "buttonUpdatePartner";
+            buttonUpdatePartner.Size = new Size(246, 58);
+            buttonUpdatePartner.TabIndex = 1;
+            buttonUpdatePartner.Text = "Редактировать партнера";
+            buttonUpdatePartner.UseVisualStyleBackColor = false;
             // 
-            // button3
+            // buttonDeletePartner
             // 
-            button3.BackColor = Color.White;
-            button3.Location = new Point(481, 5);
-            button3.Margin = new Padding(5, 5, 5, 5);
-            button3.Name = "button3";
-            button3.Size = new Size(228, 58);
-            button3.TabIndex = 2;
-            button3.Text = "button3";
-            button3.UseVisualStyleBackColor = false;
+            buttonDeletePartner.BackColor = Color.White;
+            buttonDeletePartner.Location = new Point(517, 5);
+            buttonDeletePartner.Margin = new Padding(5);
+            buttonDeletePartner.Name = "buttonDeletePartner";
+            buttonDeletePartner.Size = new Size(246, 58);
+            buttonDeletePartner.TabIndex = 2;
+            buttonDeletePartner.Text = "Удалить партнера";
+            buttonDeletePartner.UseVisualStyleBackColor = false;
             // 
             // panel1
             // 
@@ -99,31 +99,13 @@
             panel1.Size = new Size(841, 131);
             panel1.TabIndex = 1;
             // 
-            // label1
+            // label5
             // 
-            label1.Location = new Point(20, 15);
-            label1.Name = "label1";
-            label1.Size = new Size(278, 28);
-            label1.TabIndex = 0;
-            label1.Text = "Тип | Наименование партнера";
-            // 
-            // label2
-            // 
-            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            label2.Location = new Point(20, 43);
-            label2.Name = "label2";
-            label2.Size = new Size(278, 25);
-            label2.TabIndex = 1;
-            label2.Text = "Директор";
-            // 
-            // label3
-            // 
-            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            label3.Location = new Point(20, 68);
-            label3.Name = "label3";
-            label3.Size = new Size(278, 25);
-            label3.TabIndex = 2;
-            label3.Text = "+7 223 322 22 32";
+            label5.Location = new Point(709, 15);
+            label5.Name = "label5";
+            label5.Size = new Size(49, 28);
+            label5.TabIndex = 4;
+            label5.Text = "10%";
             // 
             // label4
             // 
@@ -134,13 +116,31 @@
             label4.TabIndex = 3;
             label4.Text = "Рейтинг: 10";
             // 
-            // label5
+            // label3
             // 
-            label5.Location = new Point(709, 15);
-            label5.Name = "label5";
-            label5.Size = new Size(49, 28);
-            label5.TabIndex = 4;
-            label5.Text = "10%";
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label3.Location = new Point(20, 68);
+            label3.Name = "label3";
+            label3.Size = new Size(278, 25);
+            label3.TabIndex = 2;
+            label3.Text = "+7 223 322 22 32";
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label2.Location = new Point(20, 43);
+            label2.Name = "label2";
+            label2.Size = new Size(278, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Директор";
+            // 
+            // label1
+            // 
+            label1.Location = new Point(20, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(278, 28);
+            label1.TabIndex = 0;
+            label1.Text = "Тип | Наименование партнера";
             // 
             // Form1
             // 
@@ -151,7 +151,7 @@
             Controls.Add(panel1);
             Controls.Add(flowLayoutPanel1);
             Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            Margin = new Padding(5, 5, 5, 5);
+            Margin = new Padding(5);
             Name = "Form1";
             Padding = new Padding(10);
             Text = "Партнеры";
@@ -163,9 +163,9 @@
         #endregion
 
         private FlowLayoutPanel flowLayoutPanel1;
-        private Button button1;
-        private Button button2;
-        private Button button3;
+        private Button buttonAddPartner;
+        private Button buttonUpdatePartner;
+        private Button buttonDeletePartner;
         private Panel panel1;
         private Label label1;
         private Label label4;
