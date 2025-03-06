68a3ee4e4860305ecacc79c08b2a93a80e8676e6
Курзенева Кристина <kristinakurzeneva35@gmail.com>
2025-03-06 15:57:37 +0500

    Создал макет и подключил пакеты

--- PartnersDB/Form1.Designer.cs
+++ PartnersDB/Form1.Designer.cs
@@ -28,12 +28,149 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            panel1 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            flowLayoutPanel1.SuspendLayout();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(button1);
+            flowLayoutPanel1.Controls.Add(button2);
+            flowLayoutPanel1.Controls.Add(button3);
+            flowLayoutPanel1.Dock = DockStyle.Top;
+            flowLayoutPanel1.Location = new Point(10, 10);
+            flowLayoutPanel1.Margin = new Padding(5);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(841, 71);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.White;
+            button1.Location = new Point(5, 5);
+            button1.Margin = new Padding(5, 5, 5, 5);
+            button1.Name = "button1";
+            button1.Size = new Size(228, 58);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.White;
+            button2.Location = new Point(243, 5);
+            button2.Margin = new Padding(5, 5, 5, 5);
+            button2.Name = "button2";
+            button2.Size = new Size(228, 58);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.White;
+            button3.Location = new Point(481, 5);
+            button3.Margin = new Padding(5, 5, 5, 5);
+            button3.Name = "button3";
+            button3.Size = new Size(228, 58);
+            button3.TabIndex = 2;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(10, 89);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(841, 131);
+            panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.Location = new Point(20, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(278, 28);
+            label1.TabIndex = 0;
+            label1.Text = "Тип | Наименование партнера";
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label2.Location = new Point(20, 43);
+            label2.Name = "label2";
+            label2.Size = new Size(278, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Директор";
+            // 
+            // label3
+            // 
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label3.Location = new Point(20, 68);
+            label3.Name = "label3";
+            label3.Size = new Size(278, 25);
+            label3.TabIndex = 2;
+            label3.Text = "+7 223 322 22 32";
+            // 
+            // label4
+            // 
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label4.Location = new Point(20, 93);
+            label4.Name = "label4";
+            label4.Size = new Size(278, 25);
+            label4.TabIndex = 3;
+            label4.Text = "Рейтинг: 10";
+            // 
+            // label5
+            // 
+            label5.Location = new Point(709, 15);
+            label5.Name = "label5";
+            label5.Size = new Size(49, 28);
+            label5.TabIndex = 4;
+            label5.Text = "10%";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(861, 574);
+            Controls.Add(panel1);
+            Controls.Add(flowLayoutPanel1);
+            Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            Margin = new Padding(5, 5, 5, 5);
+            Name = "Form1";
+            Padding = new Padding(10);
+            Text = "Партнеры";
+            flowLayoutPanel1.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Panel panel1;
+        private Label label1;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label5;
     }
 }
---
+++ PartnersDB/PartnersProduct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PartnersDB;
+
+public partial class PartnersProduct
+{
+    public short Id { get; set; }
+
+    public short IdPartner { get; set; }
+
+    public short IdProduct { get; set; }
+
+    public int Count { get; set; }
+
+    public DateTime DateSelling { get; set; }
+
+    public virtual Partner IdPartnerNavigation { get; set; } = null!;
+
+    public virtual Product IdProductNavigation { get; set; } = null!;
+}
